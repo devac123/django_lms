7a9a1f7bce43c3a2933f824d1b6da2583055c0fd
Your Name <you@example.com>
2024-11-24 07:18:04 -0800

    change

--- lms/settings.py
+++ lms/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 LOGIN_REDIRECT_URL = '/profile/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = '/app/staticfiles/'
 
 STATICFILES_DIRS  = [
     os.path.join(BASE_DIR, 'static')
