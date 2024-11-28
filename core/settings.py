c7440cab9014aaf754904bedd6b5b55524dcd932
Garg <63488857+devac123@users.noreply.github.com>
2024-11-28 18:07:58 +0530

    media fixed

--- core/settings.py
+++ core/settings.py
@@ -66,7 +66,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'core.context_processors.env_settings'
+                'core.context_processors.env_settings',
+                'django.template.context_processors.media'
             ],
         },
     },
