e01ca2ed14b32caf7b1cb18162067641b3677023
Your Name <you@example.com>
2024-11-29 01:22:22 -0800

    update contact us form

--- base/forms/ContactForm.py
+++ base/forms/ContactForm.py
@@ -6,3 +6,26 @@ class ContactUsForm(forms.ModelForm):
     class Meta:
         model = Contact
         fields = ['subject', 'name', 'email', 'message']
+        
+        widgets = {
+            'name': forms.TextInput(attrs={
+                'class': 'form-control', 
+                'placeholder': 'Enter your name',
+                'id' : 'name-field'
+            }),
+            'email': forms.EmailInput(attrs={
+                'class': 'form-control', 
+                'placeholder': 'Enter your email',
+                'id' : 'email-field'
+            }),
+            'message': forms.Textarea(attrs={
+                'class': 'form-control',
+                'placeholder': 'Your message',
+                'id': 'message-field'
+            }),
+            'subject': forms.TextInput(attrs={
+                'class': 'form-control', 
+                'placeholder': 'Subject of your message',
+                'id': 'subject-field'
+            }),
+        }
