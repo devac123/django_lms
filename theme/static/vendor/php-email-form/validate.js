0786a3bc136b3cf827747ca78eabfb7e5d3c4cac
Your Name <you@example.com>
2024-11-30 03:38:43 -0800

    remove dummy names

--- theme/static/vendor/php-email-form/validate.js
+++ theme/static/vendor/php-email-form/validate.js
@@ -1,7 +1,7 @@
 /**
 * PHP Email Form Validation - v3.9
-* URL: https://bootstrapmade.com/php-email-form/
-* Author: BootstrapMade.com
+* URL: https://devel.com/php-email-form/
+* Author: devel.com
 */
 (function () {
   "use strict";
