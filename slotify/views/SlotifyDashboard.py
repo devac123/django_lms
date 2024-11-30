45c2fc09e49358f17b7caa772d3d8afc7cde14d5
Your Name <you@example.com>
2024-11-30 10:25:48 -0800

    update slotify view

--- slotify/views/SlotifyDashboard.py
+++ slotify/views/SlotifyDashboard.py
@@ -5,14 +5,18 @@ from slotify.slotifyModels.SlotifyUserData import SlotifyUserData
 
 @login_required
 def SlotifyDashboard(req):
-    content = {}
-    user_id = req.user.id
-    userData = SlotifyUserData.objects.get(user_id = user_id)
-    if (userData):
-        content = {
-            'data' : userData
-        }
-        return render(req,'slotify/SlotifyDashboard.html',content)
-    else:
+    try:
+        
+        user_id = req.user.id
+        userData = SlotifyUserData.objects.get(user_id = user_id)
+        if (userData):
+            content = {
+                'data' : userData
+            }
+        return render(req,'slotify/SlotifyDashboard.html',content)    
+    except:
         return HttpResponse('Somthing went Wrong on Slotify dashbord')
+    
+    
+        
     
