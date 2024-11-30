54a678dab6932e4f6beb8e5d7cc77617888265e2
Your Name <you@example.com>
2024-11-30 10:14:20 -0800

    update slotify view

--- slotify/views/SlotifyDashboard.py
+++ slotify/views/SlotifyDashboard.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,H
 from django.contrib.auth.decorators import login_required
 from slotify.slotifyModels.SlotifyUserData import SlotifyUserData
 
@@ -10,4 +10,7 @@ def SlotifyDashboard(req):
     data = SlotifyUserData.objects.get(user_id = user_id)
     if (data):
         content['user'] = data
-    return render(req,'slotify/SlotifyDashboard.html',content)
+        return render(req,'slotify/SlotifyDashboardTest.html',data)
+    else:
+        return render(req,'slotify/SlotifyDashboardTest.html')
+    
