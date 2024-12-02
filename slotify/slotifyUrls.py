355b65595621efd60263d58dd0864f89f3728157
Garg <63488857+devac123@users.noreply.github.com>
2024-12-02 18:54:57 +0530

    issue fixed

--- slotify/slotifyUrls.py
+++ slotify/slotifyUrls.py
@@ -5,7 +5,7 @@ from slotify.views import SlotifyServiceDetail
 
 
 urlpatterns =[
-    path('slotify/',SlotifyDashboard.SlotifyDashboard, name='SlotifyDashboard')
+    path('slotify/',SlotifyDashboard.SlotifyDashboard, name='SlotifyDashboard'),
     path('services/',SlotifyServiceList.SlotifyServiceList, name='SlotifyServiceList'),
     path('service_detail/<int:id>',SlotifyServiceDetail.SlotifyServiceDetail, name='service_details')
 ]
